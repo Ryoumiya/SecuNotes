1db7b51c27a6a70e09a9661e604c8381563b2cef
Ryoumiya Haru <ryomiya@outlook.com>
2020-05-25 00:55:49 +0700

    basic stuff

--- SecuNotesXam/MainPage.xaml.cs
+++ SecuNotesXam/MainPage.xaml.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
-
+using Xamarin.Essentials;
 
 namespace SecuNotesXam
 {
@@ -17,9 +17,52 @@ namespace SecuNotesXam
         public MainPage()
         {
             InitializeComponent();
+
+            GetStorageStat();
+
+            GetDeviceID();
+        }
+        
+
+        private void GetStorageStat()
+        {
+            Task<bool> SecureCheck = CheckIfSecureStorageIsSupported();
+
+            if (SecureCheck.Result)
+            {
+                StorageStatDisplay.Text = "SecureStorage Is Supported";
+                Vibration.Vibrate(150);
+            }
+            else
+            {
+                StorageStatDisplay.Text = "SecureStorage Is NOT Supported";
+                Vibration.Vibrate();
+            }
+
         }
 
+        private static async Task<bool> CheckIfSecureStorageIsSupported()
+        {
+            try
+            {
+                await SecureStorage.SetAsync("GenericTestKey", "Sucsess!");
+                await SecureStorage.GetAsync("GenericTestKey");
+                return true;
+            }
+            catch (Exception ex)
+            {
+                // Possible that device doesn't support secure storage on device.
+                return false;
+            }
+        }
 
+        private void GetDeviceID()
+        {
+            StringBuilder StringDevice = new StringBuilder();
+            StringDevice.AppendLine("Device Model :" + DeviceInfo.Model);
+            StringDevice.AppendLine("Manufacturer :" + DeviceInfo.Manufacturer);
+            DeviceIDStatDisplay.Text = StringDevice.ToString();
+        }
 
     }
 }
--- SecuNotesXam/NotedItem.cs
+++ SecuNotesXam/NotedItem.cs
@@ -1,11 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms;
+using Newtonsoft.Json;
 
 namespace SecuNotesXam
 {
     class NotedItem
     {
+        public DateTime LastModifiedDate { get; set; }
+        public string TitleText { get; set; }
+        public string ContentText { get; set; }
+        public string PesudoID { get; set; }
+
+
+        public override string ToString()
+        {
+            return TitleText + "/:=:/" + ContentText;
+        }
+
+        public void PraseFromString(string input)
+        {
+            string[] items = input.Split(new string[] { "/:=:/" }, StringSplitOptions.None);
+            TitleText = items[0];
+            ContentText = items[1];
+        }
+
+        public string GetJSON()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+        public void PraseJSON(string input)
+        {
+            NotedItem temp = JsonConvert.DeserializeObject<NotedItem>(input);
+            LastModifiedDate = temp.LastModifiedDate;
+            TitleText = temp.TitleText;
+            ContentText = temp.ContentText;
+
+        }
 
     }
 }
